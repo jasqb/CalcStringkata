ab98f7812a2c046113146238db1f1f747d5c2399
jasman <jasper16cheat@mail.ru>
2023-05-26 14:58:04 +0300

    Initial commit

---
+++ src/Numbers.java
@@ -0,0 +1,15 @@
+//public class Numbers {
+//    public static final String[] numb = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+//
+//
+//
+//
+//
+//
+////    public static int StringToInt(String number) {
+////        for (int i = 0; i < numb.length; i++) {
+////            if (numb[i].equals(number)) return ++i;
+////        }
+////        throw new RuntimeException();
+////    }
+//}
